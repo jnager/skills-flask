9fdc1b2881a4e080c0f698fbc455438ba35318d3
jnager <julielnager@gmail.com>
2016-04-24 16:46:07 -0700

    finished Flask skills assessment

--- application.py
+++ application.py
@@ -1,18 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index_page():
-    """Show an index page."""
+    """Show the homepage."""
 
-    return "<html><body>This is the homepage.</body></html>"
+    return render_template("index.html")
 
     # Alternately, we could make this a Jinja template in `templates/`
     # and return that result of rendering this, like:
     #
     # return render_template("index.html")
+
+
+@app.route("/application-form")
+def application_form_page():
+    """Gathers info from applicant"""
+
+    return render_template("application-form.html")
+
+
+@app.route("/application-response", methods=["POST", "GET"])
+def application_response_page():
+    """Thanks user for their application."""
+
+    # Handles POST method, which should be the most common scenario.
+    if request.method == "POST":
+        fname = request.form.get("firstname")
+        lname = request.form.get("lastname")
+        role = request.form.get("role")
+        salary = int(request.form.get("salary"))
+
+        return render_template("application-response.html",
+                               firstname=fname,
+                               lastname=lname,
+                               role=role,
+                               salaryreq=salary)
+    # Handles GET method and redirects to application form page.
+    # Mostly handling the scenario where someone tries to load page directly.
+    else:
+        return redirect("/application-form")
 
 
 if __name__ == "__main__":
